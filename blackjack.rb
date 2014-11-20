f8513cc98de281f71664b8218684b2f96fe75439
Andrew Buckingham <abuckingham99@gmail.com>
2014-11-19 21:47:21 -0600

    Initial Commit

---
+++ blackjack.rb
@@ -0,0 +1,30 @@
+# Game Logic
+
+# Decks: Will I need more than one deck?
+
+# I'll need:
+# - At least one deck (A - K, four suits)
+# - A way to keep track of the user's name
+# - A way to keep track of all cards available and in use
+# - A way to force the dealer to deal at least to 17
+# - A way to set 21 as the winning hand, and a way to handle 'busts'
+# - A loop that allows the player to continue to play until he/she wants to quit
+# - A way to keep track of bets and money
+# - A way to 'shuffle' the deck
+
+# Implementation
+# Array vs. Hash for the cards
+# Using randomization                  
+
+# Bonus Features
+# - Burning cards
+# - doubling down
+# - insurance bets
+# - splitting
+
+# Prodecural Blackboard Game, written in Ruby
+Require 'pry'
+
+
+                 
+                 

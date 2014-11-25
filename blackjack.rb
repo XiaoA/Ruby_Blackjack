e7b4be5375c41c398e0fb6604ebdde3e5468b924
Andrew Buckingham <abuckingham99@gmail.com>
2014-11-25 08:26:12 -0600

    Replace 'shoe.pop.pop' with 'shoe.sample.pop'

--- blackjack.rb
+++ blackjack.rb
@@ -31,6 +31,8 @@ def say(msg)
   puts "=> #{msg}"
 end
 
+#def check_for_winner
+
 deck_one = { 'two_of_hearts' =>  2, 'three_of_hearts' => 3, 'four_of_hearts' =>  4, 'five_of_hearts' =>  5, 'six_of_hearts' =>  6, 'seven_of_hearts' =>  7, 'eight_of_hearts' =>  8, 'nine_of_hearts' =>  9, 'ten_of_hearts' =>  10, 'jack_of_hearts' =>  10, 'queen_of_hearts' => 10, 'king_of_hearts' => 10, 'ace_of_hearts' => 11, 'two_of_clubs' =>  2, 'three_of_clubs' => 3, 'four_of_clubs' =>  4, 'five_of_clubs' =>  5, 'six_of_clubs' =>  6, 'seven_of_clubs' =>  7, 'eight_of_clubs' =>  8, 'nine_of_clubs' =>  9, 'ten_of_clubs' =>  10, 'jack_of_clubs' =>  10, 'queen_of_clubs' => 10, 'king_of_clubs' => 10, 'ace_of_clubs' => 11, 'two_of_spades' =>  2, 'three_of_spades' => 3, 'four_of_spades' =>  4, 'five_of_spades' =>  5, 'six_of_spades' =>  6, 'seven_of_spades' =>  7, 'eight_of_spades' =>  8, 'nine_of_spades' =>  9, 'ten_of_spades' =>  10, 'jack_of_spades' =>  10, 'queen_of_spades' => 10, 'king_of_spades' => 10, 'ace_of_spades' => 11, 'two_of_diamonds' =>  2, 'three_of_diamonds' => 3, 'four_of_diamonds' =>  4, 'five_of_diamonds' =>  5, 'six_of_diamonds' =>  6, 'seven_of_diamonds' =>  7, 'eight_of_diamonds' =>  8, 'nine_of_diamonds' =>  9, 'ten_of_diamonds' =>  10, 'jack_of_diamonds' =>  10, 'queen_of_diamonds' => 10, 'king_of_diamonds' => 10, 'ace_of_diamonds' => 11 }
 
 deck_two = { 'two_of_hearts' =>  2, 'three_of_hearts' => 3, 'four_of_hearts' =>  4, 'five_of_hearts' =>  5, 'six_of_hearts' =>  6, 'seven_of_hearts' =>  7, 'eight_of_hearts' =>  8, 'nine_of_hearts' =>  9, 'ten_of_hearts' =>  10, 'jack_of_hearts' =>  10, 'queen_of_hearts' => 10, 'king_of_hearts' => 10, 'ace_of_hearts' => 11, 'two_of_clubs' =>  2, 'three_of_clubs' => 3, 'four_of_clubs' =>  4, 'five_of_clubs' =>  5, 'six_of_clubs' =>  6, 'seven_of_clubs' =>  7, 'eight_of_clubs' =>  8, 'nine_of_clubs' =>  9, 'ten_of_clubs' =>  10, 'jack_of_clubs' =>  10, 'queen_of_clubs' => 10, 'king_of_clubs' => 10, 'ace_of_clubs' => 11, 'two_of_spades' =>  2, 'three_of_spades' => 3, 'four_of_spades' =>  4, 'five_of_spades' =>  5, 'six_of_spades' =>  6, 'seven_of_spades' =>  7, 'eight_of_spades' =>  8, 'nine_of_spades' =>  9, 'ten_of_spades' =>  10, 'jack_of_spades' =>  10, 'queen_of_spades' => 10, 'king_of_spades' => 10, 'ace_of_spades' => 11, 'two_of_diamonds' =>  2, 'three_of_diamonds' => 3, 'four_of_diamonds' =>  4, 'five_of_diamonds' =>  5, 'six_of_diamonds' =>  6, 'seven_of_diamonds' =>  7, 'eight_of_diamonds' =>  8, 'nine_of_diamonds' =>  9, 'ten_of_diamonds' =>  10, 'jack_of_diamonds' =>  10, 'queen_of_diamonds' => 10, 'king_of_diamonds' => 10, 'ace_of_diamonds' => 11 }
@@ -45,31 +47,37 @@ burn_card_stack = []
 dealer_hand = []
 player_hand = []
 
-burn_card_stack << shoe.pop.pop
-dealer_hand << shoe.pop.pop
-player_hand << shoe.pop.pop
-dealer_hand << shoe.pop.pop
-
-
-def hit(shoe)
-  deal_player_card(shoe)
-end
-
-
-
-#def check_for_winner
-
+burn_card_stack << shoe.sample.pop
+dealer_hand << shoe.sample.pop
+player_hand << shoe.sample.pop
+dealer_hand << shoe.sample.pop
+binding.pry
+player_hand << shoe.sample.pop
 
 puts "Welcome to Blackjack!"
 puts "The #{burn_card_stack[0][0]} card has been burned!"
-puts "Dealer has a  #{dealer_hand[0][0]}"
-puts "You have a #{player_hand[0][0]}"
-say "Do you want to Hit or Stand? (Type '1' to Hit, and '2' to Stand)"
-hit_or_stand = gets.chomp.to_s
+
+#until #{dealer_hand[0][1].chomp.to_i} >= 17
+# say "Dealer has a  #{dealer_hand[0][0]}"
 
 
 
 
+  
+
+say "Dealer's hand: #{dealer_hand[0][0]} (plus hidden card)"
+say "Player's hand: #{player_hand[0][0]}"
+puts "Do you want to Hit or Stand? (Type '1' to Hit, and '2' to Stand)"
+hit_or_stand = gets.chomp.to_i
+
+if hit_or_stand == '1'
+  player_hand << shoe.sample.pop
+else
+  puts "Time to see who won!"
+end
+
+
+puts "Thanks for playing!"
 
 
 
